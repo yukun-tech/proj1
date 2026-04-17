96d1f05bb41d5e63f40175d5e20a00b3f6596ee0
Yukun Tao <yukunwpi@gmail.com>
2026-04-16 17:25:19 -0700

    feat: successfully connected to MongoDB using modular config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-
+import connectDB from './config/db.js';
 
 
 const Port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
